90e60c0e3a7ebd98607695bbfc8407bc37346e96
zyad237 <zyadadaly959@gmail.com>
2025-11-05 18:42:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ if not TOKEN:
 CONFIG = {
     "data_dir": "data",
     "database_file": "quiz_bot.db",
-    "max_questions_per_quiz": 50,
+    "max_questions_per_quiz": 100,
     "time_between_questions": 1.5,
 }
 
